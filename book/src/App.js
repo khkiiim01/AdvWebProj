3dc32ddbc31a3cc9bf571644d5f923abaae7da28
khkiiim01 <khkiiim01@gmail.com>
2024-12-05 11:13:02 +0900

    수정 완료

--- book/src/App.js
+++ book/src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="AdvWebProj">
         <div className="App">
           <Header />
           <Routes>
